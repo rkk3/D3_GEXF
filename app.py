f8016c592a35c9b174919c9d5547fbbbd20c9d5f
Robert <RKK3@Geneseo.edu>
2016-03-28 22:33:54 -0700

    testing

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, g
+
+import itertools
+import requests
+import json
+import os
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
